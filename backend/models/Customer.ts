8c648879e048e6af440986b1815f417a380e3945
Marekus <marek.stransky1@gmail.com>
2024-03-16 23:14:19 +0100

    Backend: Customer model

---
+++ backend/models/Customer.ts
@@ -0,0 +1,20 @@
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from "typeorm";
+import { Order } from './Order';
+
+@Entity()
+export class Customer {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    firstName: string;
+
+    @Column()
+    lastName: string;
+
+    @Column()
+    email: string;
+    
+    @OneToMany(() => Order, order => order.customer)
+    orders: Order[];
+}
