70d77dd7979af2e44b1cc6454823e6585a26e6cc
Marekus <marek.stransky1@gmail.com>
2024-03-16 23:14:35 +0100

    Backend: Order model

---
+++ backend/models/Order.ts
@@ -0,0 +1,24 @@
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, OneToMany } from 'typeorm';
+import { Customer } from './Customer';
+import { OrderItem } from './OrderItem';
+
+@Entity()
+export class Order {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    purchaseDate: Date;
+
+    @Column()
+    country: string;
+
+    @Column()
+    device: string;
+
+    @ManyToOne(() => Customer, customer => customer.orders)
+    customer: Customer;
+
+    @OneToMany(() => OrderItem, orderItem => orderItem.order)
+    orderItems: OrderItem[];
+}
