45e664d3c485dd09cd16a51630118069790dfc7b
Marekus <marek.stransky1@gmail.com>
2024-03-17 11:39:14 +0100

    Backend: Order Item model

---
+++ backend/src/models/OrderItem.ts
@@ -0,0 +1,20 @@
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from 'typeorm';
+import { Order } from './Order';
+
+@Entity()
+export class OrderItem {
+    @PrimaryGeneratedColumn()
+    id!: number;
+
+    @Column()
+    EAN!: string;
+
+    @Column()
+    quantity!: number;
+
+    @Column()
+    price!: number;
+
+    @ManyToOne(() => Order, order => order.orderItems)
+    order!: Order;
+}
